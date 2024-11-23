2df7adfb87cc275fef6f919d696c6cda29bef59a
Musah Morad <musahmorad01@gmail.com>
2024-11-23 12:39:50 -0500

    Add Vite integration for CSS and JS assets in layout

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -10,6 +10,7 @@
 
     <!-- Favicon -->
     <link href="{{ asset('img/favicon.ico') }}" rel="icon">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
