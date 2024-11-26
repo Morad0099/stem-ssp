50daf1ceef8c78f933b4f6778841ef409a0815a3
Musah Morad <musahmorad01@gmail.com>
2024-11-26 12:10:51 -0500

    Change date of birth input field to use date type for improved user experience

--- resources/views/modules/website/index.blade.php
+++ resources/views/modules/website/index.blade.php
@@ -602,7 +602,7 @@
                                     <!-- Date of Birth -->
                                     <div class="col-md-6">
                                         <div class="single_input">
-                                            <input type="text" name="date_of_birth" id="date_of_birth" class="dob-input"
+                                            <input type="date" name="date_of_birth"  class="dob-input"
                                             placeholder="Date of Birth (MM/DD/YYYY)" required>
                                         </div>
                                     </div> 
