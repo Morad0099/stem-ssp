9f9a15a43b3adaad5f991ccf0d355b38e09ab910
Musah Morad <musahmorad01@gmail.com>
2024-11-26 12:36:49 -0500

    Refactor date of birth input field to simplify structure and remove unnecessary placeholder script

--- resources/views/modules/website/index.blade.php
+++ resources/views/modules/website/index.blade.php
@@ -600,18 +600,17 @@
                                         </div>
                                     </div>
                                     <!-- Date of Birth -->
+                                    {{-- <div class="col-md-6">
+                                        <div class="single_input">
+                                            <input type="text" name="date_of_birth" id="date_of_birth" class="dob-input"
+                                            placeholder="Date of Birth (MM/DD/YYYY)" required>
+                                        </div>
+                                    </div>  --}}
                                     <div class="col-md-6">
                                         <div class="single_input">
-                                          <input
-                                            type="text"
-                                            name="date_of_birth"
-                                            id="date_of_birth"
-                                            class="dob-input"
-                                            placeholder="Date of Birth (MM/DD/YYYY)"
-                                            required
-                                          />
+                                            <input type="text" name="date_of_birth" placeholder="Date of Birth" required>
                                         </div>
-                                      </div>
+                                    </div>
                                       
                                     
                                     <!-- Schools -->
@@ -1020,14 +1019,6 @@
             allowInput: true // Allow manual input
         });
     });
-    document.addEventListener("DOMContentLoaded", function () {
-  const dobInput = document.getElementById("date_of_birth");
-
-  if (!dobInput.placeholder) {
-    dobInput.placeholder = "Date of Birth (MM/DD/YYYY)";
-  }
-});
-
     </script>
     <script src="js/vendor/modernizr-3.5.0.min.js"></script>
     {{-- <script src="js/vendor/jquery-1.12.4.min.js"></script> --}}
